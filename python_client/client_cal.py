f55613416880ef5fad834f420759f72770d79ee1
Ryan Herbst <rherbst@slac.stanford.edu>
2020-04-20 14:34:24 -0700

    Update

--- python_client/client_cal.py
+++ python_client/client_cal.py
@@ -12,14 +12,17 @@ convert = [None]*2
 convert[0] = ambu_control.convertArduinoHaf
 convert[1] = ambu_control.convertNpa700B02WDFlow
 
+adjust = [0] *2
+adjust[1] = (8192-8120)
+
 if sys.platform == 'linux':
-    ambu = ambu_control.AmbuControl("/dev/ttyACM0",convert=convert)
+    ambu = ambu_control.AmbuControl("/dev/ttyACM0",convert=convert,adjust=adjust)
 else:
-    ambu = ambu_control.AmbuControl("COM3",convert=convert)
+    ambu = ambu_control.AmbuControl("COM3",convert=convert,adjust=adjust)
 
-appTop = QApplication(sys.argv,refPlot=True)
+appTop = QApplication(sys.argv)
 
-guiTop = control_gui.ControlGui(ambu=ambu)
+guiTop = control_gui.ControlGui(ambu=ambu,refPlot=True)
 guiTop.setWindowTitle("PABV Control")
 guiTop.show()
 
