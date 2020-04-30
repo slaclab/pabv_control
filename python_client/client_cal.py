800580ef0b062d4b896e7a7fde0871e238d8a685
Ryan Herbst <rherbst@slac.stanford.edu>
2020-04-30 09:14:46 -0700

    Put back offset

--- python_client/client_cal.py
+++ python_client/client_cal.py
@@ -14,6 +14,7 @@ convert[0] = ambu_control.convertArduinoHaf
 convert[1] = ambu_control.convertSp110Sm02Flow
 
 adjust = [0] *2
+adjust[1] = (32768-32778)
 #adjust[1] = (8192-8098)
 
 if sys.platform == 'linux':
