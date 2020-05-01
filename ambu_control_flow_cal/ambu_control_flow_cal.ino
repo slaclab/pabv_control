c9bd6e36256d595fffe651d0ee209ced59ade8b9
Ryan Herbst <rherbst@slac.stanford.edu>
2020-05-01 13:50:58 -0700

    Update

--- ambu_control_flow_cal/ambu_control_flow_cal.ino
+++ ambu_control_flow_cal/ambu_control_flow_cal.ino
@@ -1,8 +1,7 @@
 
 #include <Wire.h>
 
-const unsigned int RelayCount     = 2;
-const unsigned int RelayPins[2]   = {2,3};
+const unsigned int RelayPin       = 2;
 const unsigned int AnalogCount    = 4;
 const unsigned int AnalogPins[4]  = {2,3,4,5};
 const unsigned int AnalogMillis   = 9;
@@ -12,7 +11,8 @@ const unsigned int DefStartThold  = 0x8a14;
 const unsigned int DefRunState    = 2;
 
 const byte I2cAddrHw  = 73;
-const byte I2cAddrNpa = 40;
+//const byte I2cAddrNpa = 40;
+const byte I2cAddrSP110 = 0x28;
 const byte I2cCmdDlc  = 0xAC; // 2 cycle average = 8ms
 
 unsigned int startThold;
@@ -41,13 +41,12 @@ char rxBuffer[50];
 char mark[20];
 char c;
 int  ret;
+int sp110Press;
 
 void setup() {
 
-   pinMode(RelayPins[0], OUTPUT);
-   pinMode(RelayPins[1], OUTPUT);
-   digitalWrite(RelayPins[0], LOW);
-   digitalWrite(RelayPins[1], HIGH);
+   pinMode(RelayPin, OUTPUT);
+   digitalWrite(RelayPin, LOW);
 
    analogTime = millis();
    relayTime  = millis();
@@ -57,10 +56,23 @@ void setup() {
    relayPeriod = DefRelayPeriod;
    relayOn     = DefRelayOn;
    startThold  = DefStartThold;
+   runState    = DefRunState;
    inhalation  = 0;
 
    Serial.begin(57600);
    Wire.begin();
+
+   // setup for the Superior SP110 sensor
+   Wire.beginTransmission(I2cAddrSP110);
+   // Sets 2cm with single point 0 offset correction
+   // 250 Hz, so it should always be faster than the 9ms AnalogMillis
+   Wire.write(0b10011101);
+   Wire.endTransmission();
+   delay(200);
+   Wire.beginTransmission(I2cAddrSP110);
+   Wire.write(0b10111101);
+   Wire.endTransmission(); // Auto-Zero is done
+   delay(200);
 }
 
 void (* resetFunc)(void) = 0;
@@ -72,19 +84,22 @@ void loop() {
 
    if ((currTime - analogTime) > AnalogMillis ) {
 
-      // Read npa sensor
-      Wire.requestFrom(I2cAddrNpa, byte(2));
-      i2cHigh = Wire.read() & 0x3F;
+
+      // Read Superior SP110 sensor, this returns a signed INT
+      Wire.requestFrom(I2cAddrSP110, byte(2));
+      i2cHigh = Wire.read();
       i2cLow = Wire.read();
+      // puts the range in uint territory for compatability w/ GUI
+      sp110Press = ((i2cHigh << 8) | i2cLow) + 32768;
 
       // Read honywell
       Wire.requestFrom(I2cAddrHw, byte(2));
       for (x=0; x < 2; x++) i2cRaw[x] = Wire.read();
 
-      sprintf(txBuffer,"STATUS %i %i %i 0x%x %i 0x%.2x%.2x 0x%.2x%.2x\n", (cycleCount/100),
+      sprintf(txBuffer,"STATUS %i %i %i 0x%x %i 0x%.2x%.2x 0x%.4x\n", (cycleCount/100),
                        relayPeriod, relayOn, startThold, runState,
                        i2cRaw[0], i2cRaw[1],
-                       i2cHigh, i2cLow);
+                       sp110Press);
 
       Serial.write(txBuffer);
       analogTime = currTime;
