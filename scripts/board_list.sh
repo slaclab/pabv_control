d2f2d4f50bd876cb6db8b9bb1c361f9d8d041826
Matthias Wittgen <wittgen@slac.stanford.edu>
2020-05-06 20:10:11 -0700

    add compile script

--- scripts/board_list.sh
+++ scripts/board_list.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/bash
 exec tools/bin/arduino-cli --config-file etc/arduino-cli.yaml board list
---
+++ scripts/compile.sh
@@ -0,0 +1,17 @@
+CLI=tools/bin/arduino-cli
+CLI_OPTS="--config-file etc/arduino-cli.yaml"
+board=arduino:avr:uno
+arduino_libs=arduino/ambu_libraries
+git_state=$((git status --porcelain | grep -q . ) && echo "-dirty" || echo "")
+git_tag_raw=$(git describe --tags)
+
+git_tag=${git_tag_raw}
+echo "const char *version_string=\""${git_tag}${git_state}"\";" >arduino/ambu_libraries/ambu_common/version.h
+echo "client_version=\""$Pgit_tag${Pgit_state}"\"" >python_client/client_version.py
+echo "client_version=\""${git_tag}${git_state}"\"" >arduino_installer/client_version.py
+arduino_sketches="ambu_control_superior ambu_control_flow_cal ambu_control_dual"
+
+for x in ${arduino_sketches}
+do
+${CLI} $CLI_OPTS --libraries ${arduino_libs}  compile  -b ${board} -o arudino/${x}/${x}.hex arduino/${x}
+done
