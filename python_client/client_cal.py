f5d307451e33d307e0394910e25197b20ae46a79
Ryan Herbst <rherbst@slac.stanford.edu>
2020-05-13 10:06:53 -0700

    Python cleanup

--- python_client/client_cal.py
+++ python_client/client_cal.py
@@ -8,15 +8,6 @@ from PyQt5.QtGui     import *
 import control_gui
 import ambu_control
 
-convert = [None]*2
-convert[0] = ambu_control.convertArduinoHaf
-#convert[1] = ambu_control.convertNpa700B02WDFlow
-convert[1] = ambu_control.convertSp110Sm02Flow
-
-adjust = [0] *2
-adjust[1] = (32768-32778)
-#adjust[1] = (8192-8098)
-
 if sys.platform == 'linux':
     ambu = ambu_control.AmbuControl("/dev/ttyACM0",convert=convert,adjust=adjust)
 else:
