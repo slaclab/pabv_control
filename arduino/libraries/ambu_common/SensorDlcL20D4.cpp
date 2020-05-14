5634b79f679ae8cbce1d574805ed14d2ce34b1ab
Ryan Herbst <rherbst@slac.stanford.edu>
2020-05-14 12:32:59 -0700

    Update

--- arduino/libraries/ambu_common/SensorDlcL20D4.cpp
+++ arduino/libraries/ambu_common/SensorDlcL20D4.cpp
@@ -21,6 +21,10 @@ void SensorDlcL20D4::update(unsigned int ctime) {
    Wire.requestFrom(addr_, byte(4));
    for (x_=0; x_ < 4; x_++) data_[x_] = Wire.read();
 
+   Serial.print("Debug: Status :");
+   Serial.print(data_[0],HEX);
+   Serial.print("\n");
+
    if ( data_[0] == 0x40 ) {
 
        raw_ = ((double)data_[1] * 65536.0) + ((double)data_[2] * 256.0) + (double)data_[3];
