3c01c4f26d9a6b33fb712c22c7c3f85332377fd5
Ryan Herbst <rherbst@slac.stanford.edu>
2020-05-14 12:27:13 -0700

    Update

--- arduino/libraries/ambu_common/SensorDlcL20D4.cpp
+++ arduino/libraries/ambu_common/SensorDlcL20D4.cpp
@@ -21,14 +21,19 @@ void SensorDlcL20D4::update(unsigned int ctime) {
    Wire.requestFrom(addr_, byte(4));
    for (x_=0; x_ < 4; x_++) data_[x_] = Wire.read();
 
-   raw_ = ((double)data_[1] * 65536.0) + ((double)data_[2] * 256.0) + (double)data_[3];
+   if ( data_[0] == 0x40 ) {
 
-   // Scaled value
-   scaled_ = 1.25 * ((raw_ - 8388608.0) / 8388608.0) * 20.0 * 2.54;
+       raw_ = ((double)data_[1] * 65536.0) + ((double)data_[2] * 256.0) + (double)data_[3];
 
-   // Start new cycle for dlc
-   Wire.beginTransmission(addr_);
-   Wire.write(DLC_L20D4_CMD); // 2 cycle average = 8ms
-   Wire.endTransmission();
+       // Scaled value
+       scaled_ = 1.25 * ((raw_ - 8388608.0) / 8388608.0) * 20.0 * 2.54;
+   }
+
+   // Start new cycle for dlc, if not busy
+   if ( (data_[0] & 0x20) != 0 ) {
+       Wire.beginTransmission(addr_);
+       Wire.write(DLC_L20D4_CMD); // 2 cycle average = 8ms
+       Wire.endTransmission();
+   }
 }
 
