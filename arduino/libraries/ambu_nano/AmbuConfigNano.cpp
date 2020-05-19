ea940bd828ded91802718ea3e887b7f3d99d77dd
Matthias Wittgen <matthias.michael.wittgen@cern.ch>
2020-05-19 13:28:25 -0400

    init config for nano

--- arduino/libraries/ambu_nano/AmbuConfigNano.cpp
+++ arduino/libraries/ambu_nano/AmbuConfigNano.cpp
@@ -10,16 +10,12 @@ AmbuConfigNano::AmbuConfigNano() : AmbuConfig () { }
 void AmbuConfigNano::setup() {
    addr_ = 0;
 
-   // Just in case the values are bad
-   if ( period_ == 0xFFFF ) period_ = 3000;
-   if ( onTime_ == 0xFFFF ) onTime_ = 1000;
-   if ( runState_ == 0xFFFF ) runState_ = StateRunOn;
-
-   // Cast double locations to int * to check for empty storage locations
-   if ( *((unsigned int *)(&startThold_)) == 0xFFFF ) startThold_ = -10.0;
-   if ( *((unsigned int *)(&stopThold_)) == 0xFFFF ) stopThold_ = 100.0;
-   if ( *((unsigned int *)(&volThold_)) == 0xFFFF ) volThold_ = 200.0;
-
+   period_ = 3000;
+   onTime_ = 1000;
+   runState_ = StateRunOn;
+   startThold_ = -10.0;
+   stopThold_ = 100.0;
+   volThold_ = 200.0;
    storeConfig();
    confTime_ = millis();
 }
