acca9f7e460709b29a6113bc206dd34dd31e706f
Ryan Herbst <rherbst@slac.stanford.edu>
2020-07-07 10:04:35 -0700

    Update ie ratio

--- arduino/libraries/ambu_common/CycleControl.cpp
+++ arduino/libraries/ambu_common/CycleControl.cpp
@@ -209,22 +209,13 @@ void CycleControl::update(uint32_t ctime) {
 
          // compute i/e ratio
          ieRatio_ = float(inhTime_) / float(ctime - stateTime_);
-
          stateTime_ = ctime;
       }
 
       // Going to off
       else if ( newState == StateOff ) {
-
-          // Transitioning from on directly to off
-          if ( state_ == StateOn ) inhTime_ = (ctime - stateTime_);
-
+         inhTime_ = (ctime - stateTime_);
          stateTime_ = ctime;
-      }
-
-      // Going to hold, store inhalation time
-      else if ( newState == StateHold ) {
-          inhTime_ = (ctime - stateTime_);
       }
 
       state_ = newState;
