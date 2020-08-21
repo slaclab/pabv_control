227ee9d1a67db3ffe17e6558dbd28198180cb1ef
Matthias Wittgen <wittgen@slac.stanford.edu>
2020-08-20 20:04:44 -0700

    call NSIS from package script

--- pkg_gui.bat
+++ pkg_gui.bat
@@ -1,2 +1,2 @@
 python -m venv env
-call env\Scripts\activate & pip3 install pyqt5 pyinstaller pyqtgraph pyserial pyqt5 & python pkg_gui.py
+call env\Scripts\activate & pip3 install pyqt5 pyinstaller pyqtgraph pyserial pyqt5 & python pkg_gui.py & "C:\Program Files (x86)\NSIS\makensis.exe" client.nsi
